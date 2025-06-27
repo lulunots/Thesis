95c91204d283040443d3168451cd078f18b265d4
lulunots <105442686+lulunots@users.noreply.github.com>
2025-06-27 13:30:30 +0200

    Delete Preprocessing directory

--- Preprocessing/Sortreads/run_ribo_sort_fastq.sh
+++
@@ -1,5 +0,0 @@
-python ribo_sort_fastq.py \
-    --modified-r2 /home/lulunots/Documents/1_ncRNA/3_fastq/PolyA_trimmed/PolyA_trimmed_195_R2.fastq \
-    --all-r2 /home/lulunots/Documents/1_ncRNA/3_fastq/ERA-MC-v195_fastq/ERA-MC-v195_NSXB231222_S63_L007_R2_001.fastq \
-    --all-r1 /home/lulunots/Documents/1_ncRNA/3_fastq/ERA-MC-v195_fastq/ERA-MC-v195_NSXB231222_S63_L007_R1_001.fastq \
-    --output /home/lulunots/Documents/1_ncRNA/3_fastq/Sorted/195
